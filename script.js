abfaa9b1146264b1c34376fcb3652f0f7eed2ecd
EV <>
2022-06-11 20:56:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+  
+    if (validateUsername(username) && validatePassword(password)) {
+      alert("Login was successful");
+    } else {
+      alert("Incorrect username or password format. Try Again");
+      return false;
+    }
+  });
+  
+  function validateUsername(username) {
+    const user = /[A-za-z][0-9]/;
+    if (username.value.match(user)) {
+        console.log(username.value);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  function validatePassword(inputtxt) {
+    var passw = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+    if (inputtxt.value.match(passw)) {
+        console.log(inputtxt.value);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+ 
+
+
+
+
